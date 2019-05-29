c17e844710a0da253744583fc4b307adaf217d7c
lining <lining_it@163.com>
2019-05-29 15:55:22 +0800

    增加描述

--- SqlBulkTools.NetStandard/Properties/AssemblyInfo.cs
+++ SqlBulkTools.NetStandard/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SqlBulkTools")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/liningit/SqlBulkTools")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SqlBulkTools")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
